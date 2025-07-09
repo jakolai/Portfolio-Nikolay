3ff6d2fda60b0f93d8b2e4ed47a81a5daa8a56a0
Niki <nikitomihov@gmail.com>
2025-07-09 18:41:15 +0200

    L Skill Issue Fixed Lol

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
+
   plugins: [react()],
   base: "/Portfolio-Nikolay"
-=======
-  plugins: [react()], 
-  base: "Portfolio-Nikolay"
->>>>>>> e0e661856a2a2e3a703a66b01db5868041e8034c
+
 })
